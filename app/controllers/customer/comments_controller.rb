1ece62ff591ba3990495d821c0fb99d726b0dd58
kanamaka <kubira12@gmail.com>
2021-11-25 06:44:59 +0000

    stop

--- app/controllers/customer/comments_controller.rb
+++ app/controllers/customer/comments_controller.rb
@@ -3,7 +3,6 @@ class Customer::CommentsController < ApplicationController
  def create
   @comment = current_customer.comments.new(comments_params.merge(opu_id: params[:opu_id]))
   @comment.save
-  
   redirect_back(fallback_location: root_path)
  end
 
